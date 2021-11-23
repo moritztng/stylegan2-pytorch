297c0094e6a4f35f522213ba8a39893bd3a8095e
moritztng <moritz.thuening@gmmail.com>
2021-11-23 17:30:53 +0000

    Add mean absolute gradients

--- stylegan2/view.py
+++ stylegan2/view.py
@@ -5,8 +5,23 @@ class View():
     def __init__(self):
         self.env = Environment(loader=PackageLoader('stylegan2'), autoescape=select_autoescape())
         self.template = self.env.get_template('template.html')
-   
-    def __call__(self, epoch, batch, mean_loss_d, mean_loss_g, images):
-        save_image(images, 'images.png')
+        self.mean_abs_grad_d = {}
+        self.mean_abs_grad_g = {}
+
+    def __call__(self, epoch, batch, mean_loss_d, mean_loss_g, images_real, images_fake, discriminator, generator):
+        save_image(images_real, 'images_real.png')
+        save_image(images_fake, 'images_fake.png')
+        self._append_grad(self.mean_abs_grad_d, discriminator)
+        self._append_grad(self.mean_abs_grad_g, generator)
         with open('view.html', 'w') as f:
-            f.write(self.template.render(epoch=epoch, batch=batch, labels=list(range(len(mean_loss_d))), mean_loss_d=mean_loss_d, mean_loss_g=mean_loss_g, images=images))
+            f.write(self.template.render(epoch=epoch, batch=batch, labels=list(range(len(mean_loss_d))), mean_loss_d=mean_loss_d, mean_loss_g=mean_loss_g, mean_abs_grad_d=self.mean_abs_grad_d, mean_abs_grad_g=self.mean_abs_grad_g))
+    
+    def _append_grad(self, grad, network):
+        for name, param in network.named_parameters():
+            if 'bias' not in name:
+                name = name.replace('.', '_')
+                mean_abs_grad = round(param.grad.abs().mean().item(), 5)
+                if name in grad:
+                    grad[name].append(mean_abs_grad)
+                else:
+                    grad[name] = [mean_abs_grad]
