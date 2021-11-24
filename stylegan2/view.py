9ba602de404a80a6ec05adcc9e6c133468dc32df
Moritz Thüning <moritz.thuening@gmail.com>
2021-11-24 08:40:44 +0000

    Move mean loss into View

--- stylegan2/view.py
+++ stylegan2/view.py
@@ -5,17 +5,30 @@ class View():
     def __init__(self):
         self.env = Environment(loader=PackageLoader('stylegan2'), autoescape=select_autoescape())
         self.template = self.env.get_template('template.html')
+        self.mean_loss_d = [] 
+        self.mean_loss_g = []
         self.mean_abs_grad_d = {}
         self.mean_abs_grad_g = {}
 
     def __call__(self, epoch, batch, mean_loss_d, mean_loss_g, images_real, images_fake, discriminator, generator):
-        save_image(images_real, 'images_real.png')
-        save_image(images_fake, 'images_fake.png')
+        self.mean_loss_d.append(mean_loss_d)
+        self.mean_loss_g.append(mean_loss_g)
         self._append_grad(self.mean_abs_grad_d, discriminator)
         self._append_grad(self.mean_abs_grad_g, generator)
+        save_image(images_real, 'images_real.png')
+        save_image(images_fake, 'images_fake.png')
         with open('view.html', 'w') as f:
-            f.write(self.template.render(epoch=epoch, batch=batch, labels=list(range(len(mean_loss_d))), mean_loss_d=mean_loss_d, mean_loss_g=mean_loss_g, mean_abs_grad_d=self.mean_abs_grad_d, mean_abs_grad_g=self.mean_abs_grad_g))
+            f.write(self.template.render(epoch=epoch, batch=batch, labels=list(range(len(self.mean_loss_d))), mean_loss_d=self.mean_loss_d, mean_loss_g=self.mean_loss_g, mean_abs_grad_d=self.mean_abs_grad_d, mean_abs_grad_g=self.mean_abs_grad_g))
     
+    def state_dict(self):
+        return {'mean_loss_d': self.mean_loss_d, 'mean_loss_g': self.mean_loss_g, 'mean_abs_grad_d': self.mean_abs_grad_d, 'mean_abs_grad_g': self.mean_abs_grad_g}
+    
+    def load_state_dict(self, state_dict):
+        self.mean_loss_d = state_dict['mean_loss_d'] 
+        self.mean_loss_g = state_dict['mean_loss_g']
+        self.mean_abs_grad_d = state_dict['mean_abs_grad_d']
+        self.mean_abs_grad_g = state_dict['mean_abs_grad_g']
+
     def _append_grad(self, grad, network):
         for name, param in network.named_parameters():
             if 'bias' not in name:
